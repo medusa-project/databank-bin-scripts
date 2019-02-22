67e9a9b651fc3e80877ecf68edcd9331e3a12116
Colleen Fallaw <mfall3@illlinois.edu>
2019-02-22 14:55:36 -0600

    refine package list

--- setup-system.sh
+++ setup-system.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# databank-bin-scripts/setupu-system.sh
+# databank-bin-scripts/setup-system.sh
 
 #This is to set up things like yum packages needed and so forth, so will
 #need to be run with sudo
@@ -9,12 +9,15 @@ YUMOPTS="--assumeyes"
 #YUMOPTS=""
 
 #install necessary os packages
+
+#assumes git is already installed, and that is how these files got onto the server
+
 #General
 YUM_PACKAGES="emacs-nox gcc nodejs monit"
 #ruby
-YUM_PACKAGES="$YUM_PACKAGES bzip2 openssl-devel readline-devel"
+YUM_PACKAGES="$YUM_PACKAGES bzip2 openssl-devel readline-devel ruby-devel"
 #needed for rails app
-YUM_PACKAGES="$YUM_PACKAGES libcurl-devel postgresql-devel gcc-c++ file-devel"
+YUM_PACKAGES="$YUM_PACKAGES libcurl-devel postgresql-devel gcc-c++ file-devel libyaml-devel zlib-devel libffi-devel sqlite-devel"
 yum $YUMOPTS install $YUM_PACKAGES
 
 #I didn't get the necessary g++ until I installed this
