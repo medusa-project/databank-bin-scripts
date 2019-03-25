c1141123cf7ab53d4f1ac787a515c1e9e81ebced
Troy, Joseph M <jmtroy2@illinois.edu>
2019-03-25 10:44:57 -0500

    add get_medusa_messages.sh

---
+++ get_medusa_messages.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#get_medusa_messages.sh
+HOME=/home/databank
+source $HOME/bin/env.sh
+cd $HOME/current
+if [ -d $HOME/.rbenv ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+  rbenv local 2.5.1
+fi
+bundle exec rake medusa:get_medusa_ingest_responses >> /dev/null
