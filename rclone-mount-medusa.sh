8b0e605249a955cabda9e9cde1c42b67da14000b
Colleen Fallaw <mfall3@illinois.edu>
2019-09-09 13:32:22 -0500

    fix typo

--- rclone-mount-medusa.sh
+++ rclone-mount-medusa.sh
@@ -8,4 +8,4 @@ mkdir -p $RCLONE_PIDDIR
 #mountpoint -q $RCLONE_MOUNTPOINT && fusermount -uz $RCLONE_MOUNTPOINT
 fusermount -uz $RCLONE_MEDUSA_MOUNTPOINT || echo "$RCLONE_MEDUSA_MOUNTPOINT may not be mounted currently"
 rclone mount $RCLONE_REMOTE:$RCLONE_MEDUSA_BUCKET $RCLONE_MEDUSA_MOUNTPOINT --read-only &
-echo $! > $RCLONE__MEDUSA_PIDFILE
+echo $! > $RCLONE_MEDUSA_PIDFILE
