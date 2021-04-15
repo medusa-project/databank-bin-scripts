49d4a78ce541c445455e117521ae0a5d28f7ab65
Colleen Fallaw <mfall3@illinois.edu>
2021-04-15 13:35:31 -0500

    add globus script

---
+++ scripts/globus.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# globus.sh
+HOME=/home/databank
+source $HOME/bin/env.sh
+cd $HOME/current
+if [ -d $HOME/.rbenv ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+fi
+bundle exec rails globus:copy_datasets >> /dev/null
